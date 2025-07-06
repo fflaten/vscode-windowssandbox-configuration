eb824e0af3575f10f30fc07ba013636d977a5ddf
Frode Flaten <3436158+fflaten@users.noreply.github.com>
2025-07-06 17:48:24 +0000

    Add interface for redhat xml extension API

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,16 @@
 import * as vscode from 'vscode';
+import { XMLExtensionApi } from './xmlExtensionsApi';
 
 export async function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "windowssandbox-configuration" is now active!');
-
   // Register xml schema for *.wsb files
-  const xmlExtension = vscode.extensions.getExtension('redhat.vscode-xml');
-  if (xmlExtension) {
-    if (!xmlExtension.isActive) {
-      await xmlExtension.activate();
-    }
-
-    const xmlAPI = xmlExtension.exports;
-    if (xmlAPI && xmlAPI.addXMLFileAssociations) {
-      xmlAPI.addXMLFileAssociations([{
+  const xmlExtension = await vscode.extensions.getExtension<XMLExtensionApi>('redhat.vscode-xml')?.activate();
+  if (xmlExtension && xmlExtension.addXMLFileAssociations) {
+    xmlExtension.addXMLFileAssociations([
+      {
         pattern: "**/*.wsb",
         systemId: context.asAbsolutePath("schemas/configuration.xsd")
-      }]);
-    }
+      }
+    ]);
   }
 }
 
